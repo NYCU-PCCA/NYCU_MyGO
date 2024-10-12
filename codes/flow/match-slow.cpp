b4d6bb0679df2d4fb25bd3bcdc0bb784553a2293
SorahISA <willy097212@gmail.com>
2024-10-12 23:43:44 +0800

    add flow to content

--- codes/flow/match-slow.cpp
+++ codes/flow/match-slow.cpp
@@ -1,17 +1,17 @@
-array<int,SZ> mp;
-array<bool,SZ> vis;
+array<int, sz> match;
+array<bool, sz> vis;
 bool dfs(int now) {
     if(vis[now]) return false;
     vis[now] = true;
     for(int i = 0; i < n; i++) {
         if(!G[now][i]) continue;
-        if(mp[i] == -1 or dfs(mp[i]))
-            return mp[i] = now , true;
+        if(match[i] == -1 or dfs(match[i]))
+            return match[i] = now, true;
     }
     return false;
 }
 int solve() {
-    mp.fill(-1);
+    match.fill(-1);
     int r = 0;
     for(int i = 0; i < n; i++) {
         vis.fill(false);
