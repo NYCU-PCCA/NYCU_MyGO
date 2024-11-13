e3bfae7e2ec668772b4722003566ea5dc3ccc6de
SorahISA <46983782+SorahISA@users.noreply.github.com>
2024-11-14 01:04:31 +0800

    Fixed NTT

--- codes/polynomial/fft-ckiseki.hpp
+++ codes/polynomial/fft-ckiseki.hpp
@@ -29,7 +29,7 @@ void fft(vector<complex<double>> &a, bool inv) {
 // 51*(2^25)+1, 1711276033, 29
 void ntt(vector<Mod> &a, bool inv, Mod primitive_root){
   int n = a.size();
-  Mod root = primitive_root.pow(MOD - 1) / n;
+  Mod root = primitive_root.pow((MOD - 1) / n);
   vector<Mod> rt(n + 1, 1);
   for (int i = 0; i < n; i++) rt[i + 1] = rt[i] * root;
   fft_(n, a, rt, inv);
