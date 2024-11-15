3990b2a4e7fe5187b55b0ec4ac3904eeea119782
Allen Guan <me@nella17.tw>
2024-11-15 15:21:06 +0800

    update t.sh

--- codes/basic/t.sh
+++ codes/basic/t.sh
@@ -6,15 +6,9 @@ mkdir -p .t
 p=${f:0:1}
 cp -n $f .t/$f.$hf
 case $ext in
-  cpp)
-    r=(~/r.sh $f)
-    ;;
-  py)
-    r=(python3 $f)
-    ;;
-  *)
-    exit -1
-    ;;
+  cpp)  r=(~/r.sh $f)   ;;
+  py)   r=(python3 $f)  ;;
+  *)    exit -1         ;;
 esac
 for i in $p/*.in; do
   a=${i/.in/.ans}
