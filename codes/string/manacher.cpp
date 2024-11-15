794a2208db121df3297faa0892f756fdf191f290
Allen Guan <me@nella17.tw>
2024-11-15 13:34:47 +0800

    update manacher

--- codes/string/manacher.cpp
+++ codes/string/manacher.cpp
@@ -1,15 +1,18 @@
-int z[maxn]; // 0-base
-/* center i: radius z[i * 2 + 1] / 2
-   center i, i + 1: radius z[i * 2 + 2] / 2
-   both aba, abba have radius 2 */
-void Manacher(string tmp) {
-  string s = "%";
-  int l = 0, r = 0;
-  for (char c : tmp) s.eb(c), s.eb('%');
-  for (int i = 0; i < SZ(s); ++i) {
-    z[i] = r > i ? min(z[2 * l - i], r - i) : 1;
-    while (i - z[i] >= 0 && i + z[i] < SZ(s) 
-           && s[i + z[i]] == s[i - z[i]]) ++z[i];
-    if (z[i] + i > r) r = z[i] + i, l = i;
+vector<int> manacher(const string &S) {
+  const int n = (int)S.size(), m = n * 2 + 1;
+  vector<int> z(m); string t = "%";
+  t.reserve(m); for(char c: S) t += c, t += '%';
+  for (int i = 1, l = 0, r = 0; i < m; ++i) {
+    z[i] = (r > i ? min(z[2 * l - i], r - i) : 1);
+    while (i - z[i] >= 0 and i + z[i] < m) {
+      if (t[i - z[i]] == t[i + z[i]]) z[i]++;
+      else break;
+    }
+    if (i + z[i] > r) r = i + z[i], l = i;
   }
+  return z; // the palindrome lengths are z[i] - 1
 }
+/* for (int i = 1; i + 1 < m; ++i) {
+   int l = (i - z[i] + 2) / 2, r = (i + z[i]) / 2;
+   if (l != r) // [l, r) is maximal palindrome
+} */
