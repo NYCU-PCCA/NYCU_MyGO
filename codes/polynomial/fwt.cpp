44e3a68c8e0d880b1d20c6ec49d631d179b9af57
SorahISA <willy097212@gmail.com>
2024-11-16 05:45:55 +0800

    refactor fwt

--- codes/polynomial/fwt.cpp
+++ codes/polynomial/fwt.cpp
@@ -1,16 +1,16 @@
-/* x: a[j], y: a[j + (L >> 1)]
+/* x: a[i+j], y: a[i+j+L/2]
 or: (y += x * op), and: (x += y * op)
 xor: (x, y = (x + y) * op, (x - y) * op)
 invop: or, and, xor = -1, -1, 1/2 */
-void fwt(int *a, int n, int op) { //or
+void fwt(int *a, int n, int op) { // or
   for (int L = 2; L <= n; L <<= 1)
     for (int i = 0; i < n; i += L)
-      for (int j = i; j < i + (L >> 1); ++j)
-        a[j + (L >> 1)] += a[j] * op;
+      for (int j = 0; j < L/2; ++j)
+        a[i+j+L/2] = (a[i+j+L/2] + a[i+j] * op) % mod;
 }
-constexpr int N = 21, P2N = 1 << N;
-int f[N][P2N], g[N][P2N], h[N][P2N], ct[P2N];
-void subset_convolution(int *a, int *b, int *c, int L){
+constexpr int MAXN = 20 + 1, P2N = 1 << MAXN;
+int f[MAXN][P2N], g[MAXN][P2N], h[MAXN][P2N], ct[P2N];
+void subset_convolution(int *a, int *b, int *c, int L) {
   // c_k = \sum_{i | j = k, i & j = 0} a_i * b_j
   int n = 1 << L;
   for (int i = 1; i < n; ++i)
@@ -22,9 +22,9 @@ void subset_convolution(int *a, int *b, int *c, int L){
   for (int i = 0; i <= L; ++i)
     for (int j = 0; j <= i; ++j)
       for (int x = 0; x < n; ++x)
-        h[i][x] += f[j][x] * g[i - j][x];
+        h[i][x] = (h[i][x] + f[j][x] * g[i-j][x]) % mod;
   for (int i = 0; i <= L; ++i)
-    fwt(h[i], n, -1);
+    fwt(h[i], n, mod-1);
   for (int i = 0; i < n; ++i)
     c[i] = h[ct[i]][i];
 }
