f693949955ba36a0ef6d615d92ea478e097865ba
Allen Guan <me@nella17.tw>
2024-11-15 15:50:35 +0800

    refactor mcmf

--- codes/flow/mcmf_dijkstra.cpp
+++ codes/flow/mcmf_dijkstra.cpp
@@ -1,15 +1,18 @@
-struct MinCostMaxFlow { // 0-base
+template<int MAXV, typename T = ll,
+  T INF = numeric_limits<T>::max()>
+struct MCMF { // 0-base
   struct Edge {
-    int from, to, cap, flow, cost, rev;
-  } *past[MAXN];
-  vector<Edge> G[MAXN];
-  int inq[MAXN], n, s, t;
-  int dis[MAXN], up[MAXN], pot[MAXN];
+    int from, to, rev;
+    T cap, flow, cost;
+  } *past[MAXV];
+  vector<Edge> G[MAXV];
+  bitset<MAXV> inq; int n, s, t;
+  T dis[MAXV], up[MAXV], pot[MAXV];
   bool BellmanFord() {
     fill_n(dis, n, INF), fill_n(inq, n, 0);
     queue<int> q;
-    auto relax = [&](int u, int d, int cap, Edge *e) {
-      if (cap > 0 && dis[u] > d) {
+    auto relax = [&](int u, T d, T cap, Edge *e) {
+      if (cap > 0 and dis[u] > d) {
         dis[u] = d, up[u] = cap, past[u] = e;
         if (!inq[u]) inq[u] = 1, q.push(u);
       }
@@ -19,8 +22,8 @@ struct MinCostMaxFlow { // 0-base
       int u = q.front();
       q.pop(), inq[u] = 0;
       for (auto &e : G[u]) {
-        int d2 = dis[u] + e.cost + pot[u] - pot[e.to];
-        relax(e.to, d2, min(up[u], e.cap - e.flow), &e);
+        T d2 = dis[u] + e.cost + pot[u] - pot[e.to];
+        relax(e.to, d2, min(up[u], e.cap-e.flow), &e);
       }
     }
     return dis[t] != INF;
@@ -28,8 +31,8 @@ struct MinCostMaxFlow { // 0-base
   bool Dijkstra() {
     fill_n(dis, n, INF);
     MinHeap<pii> pq;
-    auto relax = [&](int u, int d, int cap, Edge *e) {
-      if (cap > 0 && dis[u] > d) {
+    auto relax = [&](int u, T d, T cap, Edge *e) {
+      if (cap > 0 and dis[u] > d) {
         dis[u] = d, up[u] = cap, past[u] = e;
         pq.ee(pii(d, u));
       }
@@ -40,30 +43,31 @@ struct MinCostMaxFlow { // 0-base
       pq.pop();
       if (dis[u] != d) continue;
       for (auto &e : G[u]) {
-        int d2 = dis[u] + e.cost + pot[u] - pot[e.to];
-        relax(e.to, d2, min(up[u], e.cap - e.flow), &e);
+        T d2 = dis[u] + e.cost + pot[u] - pot[e.to];
+        relax(e.to, d2, min(up[u], e.cap-e.flow), &e);
       }
     }
     return dis[t] != INF;
   }
-  void solve(int _s, int _t, int &flow, int &cost, bool neg = true) {
-    s = _s, t = _t, flow = 0, cost = 0;
+  auto solve(int _s, int _t, bool neg = true) {
+    s = _s, t = _t; T flow = 0, cost = 0;
     if (neg) BellmanFord(), copy_n(dis, n, pot);
     for (; Dijkstra() /* BF */; copy_n(dis, n, pot)) {
-      for (int i = 0; i < n; ++i) dis[i] += pot[i] - pot[s];
+      for (int i=0; i < n; ++i) dis[i]+=pot[i]-pot[s];
       flow += up[t], cost += up[t] * dis[t];
       for (int i = t; past[i]; i = past[i]->from) {
         auto &e = *past[i];
         e.flow += up[t], G[e.to][e.rev].flow -= up[t];
       }
     }
+    return pair{ flow, cost };
   }
   void init(int _n) {
     n = _n, fill_n(pot, n, 0);
     for (int i = 0; i < n; ++i) G[i].clear();
   }
-  void add_edge(int a, int b, int cap, int cost) {
-    G[a].eb(Edge{a, b, cap, 0, cost, SZ(G[b])});
-    G[b].eb(Edge{b, a, 0, 0, -cost, SZ(G[a]) - 1});
+  void add_edge(int a, int b, T cap, T c) {
+    G[a].eb(edge{ a, b, SZ(G[b]), cap, 0, c });
+    G[b].eb(edge{ b, a, SZ(G[a])-1, 0, 0, -c });
   }
 };
