c95ef67c74ba7698a03d89bd0a24e4d5009a3992
Allen Guan <me@nella17.tw>
2024-11-15 10:18:48 +0800

    comment BoundedFlow -> Dinic

--- codes/flow/bounded_flow.cpp
+++ codes/flow/bounded_flow.cpp
@@ -1,28 +1,26 @@
-struct BoundedFlow { // 0-base
-  struct edge {
-    int to, cap, flow, rev;
-  };
-  vector<edge> G[N];
-  int n, s, t, dis[N], cur[N], cnt[N];
+struct Dinic /* BoundedFlow */ { // 0-base, MAXN, INF
+  struct edge { int to, cap, flow, rev; };
+  vector<edge> G[MAXN];
+  int n, s, t, dis[MAXN], cur[MAXN], cnt[MAXN];
   void init(int _n) {
     n = _n;
     for (int i = 0; i < n + 2; ++i)
       G[i].clear(), cnt[i] = 0;
   }
-  void add_edge(int u, int v, int lcap, int rcap) {
+/*void add_edge(int u, int v, int lcap, int rcap) {
     cnt[u] -= lcap, cnt[v] += lcap;
     G[u].eb(edge{v, rcap, lcap, SZ(G[v])});
     G[v].eb(edge{u, 0, 0, SZ(G[u]) - 1});
-  }
+  } */
   void add_edge(int u, int v, int cap) {
-    G[u].eb(edge{v, cap, 0, SZ(G[v])});
-    G[v].eb(edge{u, 0, 0, SZ(G[u]) - 1});
+    G[u].eb(edge{ v, cap, 0, SZ(G[v]) });
+    G[v].eb(edge{ u, 0, 0, SZ(G[u]) - 1 });
   }
   int dfs(int u, int cap) {
-    if (u == t || !cap) return cap;
+    if (u == t or !cap) return cap;
     for (int &i = cur[u]; i < SZ(G[u]); ++i) {
       edge &e = G[u][i];
-      if (dis[e.to] == dis[u] + 1 && e.cap != e.flow) {
+      if (dis[e.to] == dis[u] + 1 and e.cap != e.flow) {
         int df = dfs(e.to, min(e.cap - e.flow, cap));
         if (df) {
           e.flow += df, G[e.to][e.rev].flow -= df;
@@ -35,13 +33,12 @@ struct BoundedFlow { // 0-base
   }
   bool bfs() {
     fill_n(dis, n + 3, -1);
-    queue<int> q;
-    q.push(s), dis[s] = 0;
+    queue<int> q; q.push(s), dis[s] = 0;
     while (!q.empty()) {
       int u = q.front();
       q.pop();
       for (edge &e : G[u])
-        if (!~dis[e.to] && e.flow != e.cap)
+        if (dis[e.to] == -1 and e.flow != e.cap)
           q.push(e.to), dis[e.to] = dis[u] + 1;
     }
     return dis[t] != -1;
@@ -55,7 +52,7 @@ struct BoundedFlow { // 0-base
     }
     return flow;
   }
-  bool solve() {
+/*bool solve() {
     int sum = 0;
     for (int i = 0; i < n; ++i)
       if (cnt[i] > 0)
@@ -74,5 +71,5 @@ struct BoundedFlow { // 0-base
     if (!solve()) return -1; // invalid flow
     int x = G[_t].back().flow;
     return G[_t].pb(), G[_s].pb(), x;
-  }
+  } */
 };
